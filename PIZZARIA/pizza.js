d2e3b9df630aa589c05bd2357317f00c7005851f
juliosena <juliosena.1411@gmail.com>
2025-05-08 16:53:39 -0300

    22

--- PIZZARIA/pizza.js
+++ PIZZARIA/pizza.js
@@ -1,13 +1,1 @@
-let pizzas = [];
-let pizzaParaAlterar = null;
-
-function mostrarSecao(secao) {
-  // Esconde todas as seções
-  document.getElementById("pizzas").classList.add("hidden");
-  document.getElementById("consulta").classList.add("hidden");
-  document.getElementById("alterar").classList.add("hidden");
-  document.getElementById("vendas").classList.add("hidden");
-
-  // Mostra a seção selecionada
-  document.getElementById(secao).classList.remove("hidden");
-}
+let 
