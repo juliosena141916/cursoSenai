4e1e2a48a2e14899180b3e10419e0be54908087d
juliosena <juliosena.1411@gmail.com>
2025-05-15 14:59:51 -0300

    poiu

--- PIZZARIA/pizza.js
+++ PIZZARIA/pizza.js
@@ -1,1 +1,15 @@
-let 
+function mostrarSecao(secao){
+    // Esconde todas as seções
+    document.getElementById("cadastrarPizza").classList.add("hidden");
+    document.getElementById("consultarPizza").classList.add("hidden");
+    // document.getElementById("removerPizza").classList.add("hidden");
+    
+    // Mostra a seção
+    document.getElementById(secao).classList.remove("hidden");
+}
+
+function cadastrarPizza(){
+    const pizza = document.getElementById("nome").value;
+    const ingredientes = document.getElementById("ingredientes").value;
+    const preco = parseFloat(document.getElementById("preco").value);
+}
