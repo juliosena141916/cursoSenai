18705a195b629120418c21c5f37485422cccd9b9
juliosena <juliosena.1411@gmail.com>
2025-05-15 16:53:54 -0300

    poiu

--- PIZZARIA/pizza.js
+++ PIZZARIA/pizza.js
@@ -1,3 +1,6 @@
+let pizzas = [];
+let pizzaParaAlterar = null;
+
 function mostrarSecao(secao){
     // Esconde todas as seções
     document.getElementById("cadastrarPizza").classList.add("hidden");
@@ -12,4 +15,35 @@ function cadastrarPizza(){
     const pizza = document.getElementById("nome").value;
     const ingredientes = document.getElementById("ingredientes").value;
     const preco = parseFloat(document.getElementById("preco").value);
+
+    if (pizza && ingredientes && preco) {
+        const novaPizza = {
+            nome: pizza,
+            ingredientes: ingredientes,
+            preco: preco
+        };
+        pizzas.push(novaPizza);
+        document.getElementById("nome").value = "";
+        document.getElementById("ingredientes").value = "";
+        document.getElementById("preco").value = "";
+        atualizarLista();
+        alert("Pizza cadastrada com sucesso!");        
+}else{
+        alert("Por favor, preencha todos os campos.");
+    }
+}
+
+function atualizarLista(lista = pizzas){
+    const tabela = document.getElementById("tabelaPizzas");
+    tabela.innerHTML = "";
+
+    lista.forEach((pizza) => {
+        const linha = document.createElement("tr");
+        linha.innerHTML = `
+            <td>${pizza.nome}</td>
+            <td>${pizza.ingredientes}</td>
+            <td>${pizza.preco.toFixed(2)}</td>
+        `;
+        tabela.appendChild(linha);
+    });
 }
